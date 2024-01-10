2429bf25133deed89d756819aed19b5227d7c364
Chisfx <101854771+Chisfx@users.noreply.github.com>
2024-01-10 02:36:21 -0600

    Creación de capa Infrastructure la cual tiene como dependencia a Application y se encuentran el DbContext, migraciones, repositorios y otros recursos

---
+++ App.Infrastructure/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using App.Application.Interfaces.Contexts;
+using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Data;
+
+namespace App.Infrastructure.DbContexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public IDbConnection Connection => Database.GetDbConnection();
+        public bool HasChanges => ChangeTracker.HasChanges();
+        public DatabaseFacade DataBase => Database;
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.HasDefaultSchema("dbo");
+
+            builder.Entity<User>(entity =>
+            {
+                entity.ToTable(name: "Users");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            });
+        }
+    }
+}
---
+++ App.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using App.Infrastructure.DbContexts;
+using App.Infrastructure.Repositories;
+using App.Infrastructure.Shared;
+using App.Application.Interfaces.Contexts;
+using App.Application.Interfaces.Repositories;
+using App.Application.Interfaces.Shared;
+using Microsoft.Extensions.DependencyInjection;
+namespace App.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructureLayer(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddTransient(typeof(IRepositoryAsync<>), typeof(RepositoryAsync<>));
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<ICompareObject, CompareObjectService>();
+        }
+    }
+}
