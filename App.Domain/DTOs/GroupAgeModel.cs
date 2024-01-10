4022ef9fde7c5c527e5bdd7a685cf35865912c7b
Chisfx <101854771+Chisfx@users.noreply.github.com>
2024-01-10 02:26:07 -0600

    Creación de capa Domain donde se encuentras las entidades y objetos de transferencia de datos

---
+++ App.Domain/DTOs/GroupAgeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Domain.DTOs
+{
+    public class GroupAgeModel
+    {
+        public int Age { get; set; }
+        public int Count { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ App.Domain/DTOs/GroupHostModel.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Domain.DTOs
+{
+    public class GroupHostModel
+    {
+        public string Host { get; set; }
+        public int Count { get; set; }
+        public string Color { get; set; }
+    }
+}
